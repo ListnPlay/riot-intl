aab31b4b5129f70a82765626176e2accbd2d2132
Avner Peled <avnerus@gmail.com>
2015-05-06 18:11:57 +0300

    riot hello world

--- src/riot.js
+++ src/riot.js
@@ -1,7 +1,7 @@
-/* global React */
+/* global riot */
 /* jshint esnext:true */
 
 // TODO: Remove the global `React` binding lookup once the ES6 Module Transpiler
 // supports external modules. This is a hack for now that provides the local
 // modules a referece to React.
-export default React;
+export default riot;
